e4336133bc64e9a1cdefb74a8840e2bad4beb21e
t-fujii <t-fujii@itecgr.co.jp>
2023-12-21 14:15:48 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tictactoegame/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.tictactoegame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.tictactoegame.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    var player = "p1"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.b1.setOnClickListener {
+            buttonclick(binding.b1)
+        }
+        binding.b2.setOnClickListener {
+            buttonclick(binding.b2)
+        }
+        binding.b3.setOnClickListener {
+            buttonclick(binding.b3)
+        }
+        binding.b4.setOnClickListener {
+            buttonclick(binding.b4)
+        }
+        binding.b5.setOnClickListener {
+            buttonclick(binding.b5)
+        }
+        binding.b6.setOnClickListener {
+            buttonclick(binding.b6)
+        }
+        binding.b7.setOnClickListener {
+            buttonclick(binding.b7)
+        }
+        binding.b8.setOnClickListener {
+            buttonclick(binding.b8)
+        }
+        binding.b9.setOnClickListener {
+            buttonclick(binding.b9)
+        }
+    }
+
+    fun buttonclick(btn: Button) {
+        if (btn.text == "") {
+            if (player == "p1") {
+                player = "p2"
+                btn.text = "X"
+            } else {
+                player = "p1"
+                btn.text = "O"
+            }
+        }
+    }
+
+    fun win() {
+        if (binding.b1.text == "X" && binding.b2.text == "X" && binding.b3.text == "X") {
+            toast("X won the Game")
+        }
+        if (binding.b1.text == "O" && binding.b2.text == "O" && binding.b3.text == "O") {
+            toast("O won the Game")
+        }
+        if (binding.b4.text == "X" && binding.b5.text == "X" && binding.b6.text == "X") {
+            toast("X won the Game")
+        }
+        if (binding.b4.text == "O" && binding.b5.text == "O" && binding.b6.text == "O") {
+            toast("O won the Game")
+        }
+        if (binding.b7.text == "X" && binding.b8.text == "X" && binding.b9.text == "X") {
+            toast("X won the Game")
+        }
+        if (binding.b7.text == "O" && binding.b8.text == "O" && binding.b9.text == "O") {
+            toast("X won the Game")
+        }
+
+    }
+
+    fun toast(msg: String) {
+        Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
+    }
+}
